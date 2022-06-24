99a74f41d35b3f454d0ef0cedd44aed722190ead
adenildoas1 <adenildosantosbr@hotmail.com>
2022-06-24 17:25:58 -0300

    projeto-accordion

---
+++ Accordion/index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Accordion - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Secção 1</h3>
+  <div>
+    <p>
+   Primeiro texto para analisar se o processo do Accordion esta funcionando corretamente:
+   
+   Ele ouviu passos silenciosos atrás dele. Isso não estava certo. Quem poderia estar lhe seguindo a esta hora da noite e nesta parte isolada da cidade? 
+   E justo neste exato momento em que ele iria dar o seu grande golpe. 
+   Haveria outro safado que teria tido a mesma idéia, e estaria agora observando ele e esperando uma chance de se apropriar do fruto de seu trabalho?
+    </p>
+  </div>
+  <h3>Secção 2</h3>
+  <div>
+    <p>
+    Texto número dois (2), para analisar se deu certo o processo:
+	
+	Ou os passos atrás dele significariam que um dos muitos oficiais da lei da cidade estava atrás dele e somente esperando para colocar-lhe as algemas? De repente, 
+	ele olhou em volta e notou a viela. Como um raio, ele fugiu e desapareceu pela esquerda entre os dois armazéns,
+	quase caindo em cima da lata de lixo que estava no meio da calçada. 
+    </p>
+  </div>
+  <h3>Secção 3</h3>
+  <div>
+    <p>
+    Terceiro texto para análise:
+	
+	Ele foi tateando pelo caminho no meio do breu da escuridão e de repente ficou paralisado: era um beco sem saída, ele teria de voltar por onde tinha vindo.
+	Os passos foram ficando mais e mais altos, ele viu o perfil de uma figura virando a esquina. Este era o fim da linha? Ele pensou, se colando na parede, 
+	tentando se fazer invisível no escuro, teria todo o seu planejamento ido por água a baixo? 
+	O suor lhe escorria agora, frio e molhado, ele sentia o cheiro do medo vir de sua roupa.
+    </p>
+    <ul>
+      <li>Lista número 1</li>
+      <li>Lista número 2</li>
+      <li>Lista número 3</li>
+    </ul>
+  </div>
+  <h3>Secção 4</h3>
+  <div>
+    <p>
+    Este é o quarto (4) texto:
+	
+	Ele foi tateando pelo caminho no meio do breu da escuridão e de repente ficou paralisado: era um beco sem saída, ele teria de voltar por onde tinha vindo. 
+	Os passos foram ficando mais e mais altos, ele viu o perfil de uma figura virando a esquina. Este era o fim da linha? Ele pensou, se colando na parede,
+	tentando se fazer invisível no escuro, teria todo o seu planejamento ido por água a baixo? O suor lhe escorria agora, frio e molhado, ele sentia o cheiro do medo vir de sua roupa.
+    </p>
+    <p>
+   Vou deixar estes aqui como exemplos para teste.
+    </p>
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,7 @@
+# Primeiro projeto com jQuery:
+
+
+
+## Este foi a elaboração de um Accordion utilizando o Notepad ++ e a biblioteca jQuery para sua síntese.
+
+### Utilizando um texto padrão do LIbreOffice Write, junto com o Notepad ++ e a biblioteca jQuery, criei um Accordion como uma atividade prática para testar meus conhecimentos.
